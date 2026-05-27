1efd89008d85d168eebfc27540ca0dddb4cc0acd
maanas <ec24bt023@iitdh.ac.in>
2026-05-27 23:38:10 +0530

    Added Program Counter

---
+++ rtl/core/program_counter.v
@@ -0,0 +1,18 @@
+module program_counter (
+    input clk, rst,
+    input [31:0] pc_next,
+
+    output reg [31:0] pc
+);
+
+    always @(posedge clk or posedge rst) begin
+
+        if (rst)
+            pc <= 32'b0;
+
+        else
+            pc <= pc_next;
+
+    end
+
+endmodule
