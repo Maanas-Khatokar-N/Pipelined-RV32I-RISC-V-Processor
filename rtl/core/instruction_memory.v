fd1f6bc2dacdc7587d533dac0155f5c35cbb698f
Maanas-Khatokar-N <ec24bt023@iitdh.ac.in>
2026-09-18 19:03:38 +0530

    Fix comment for memory array addressing clarification

--- rtl/core/instruction_memory.v
+++ rtl/core/instruction_memory.v
@@ -6,7 +6,7 @@ module instruction_memory (
     reg [31:0] memory [0:255];
 
 
-    //RISC-V addresses bytes. But your memory array is word-based (4 bytes).
+    //RISC-V addresses bytes. But here memory array is word-based (4 bytes).
     //So pc[31:2] is used instead of pc
     
     assign inst = memory[pc[31:2]];
